f9de1270262c6456452c14de1462640ec1243bfb
Uheeking <wert1847@naver.com>
2022-08-12 17:26:06 +0900

    signup 페이지 수정

--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,14 +1,19 @@
 import React from "react";
+import Head from 'next/head'
+import {Form} from 'ant'
 import AppLayout from "../components/AppLayout";
 
 const Signup = () => {
+  const onSubmit
   return (
-    <>
+    <AppLayout>
       <Head>
         <title>내 회원가입 | NodeBird</title>
       </Head>
-      <AppLayout>회원가입</AppLayout>
-    </>
+      <Form onFinish={onSubmit}>
+
+      </Form>
+    </AppLayout>
   );
 };
 export default Signup;
